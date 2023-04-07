3f18872c57534cc0dbd4c2cb34a4c2e9b2478667
夏金英 <jemma@xiajinyingdeMacBook-Air.local>
2023-04-07 21:35:43 +0800

    Initial commit

---
+++ Lecture/w1.py
@@ -0,0 +1,3 @@
+name = input("Who's there? ")
+msg = "Sorry " + name + ". I thought you were someone else!"
+print(msg)
---
+++ Lecture/w2.py
@@ -0,0 +1,5 @@
+asx_value=7111.4
+date='2021-01-25'
+units=1
+currency='AUD'
+print(f'The closing value of {units} unit of the All Ordinaries on {date} was {asx_value} {currency}.')
